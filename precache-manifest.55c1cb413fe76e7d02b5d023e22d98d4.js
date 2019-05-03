eec9607dd302b4a26b754a72f46c2c7deaabdaef
Thom-Roberts <robertsorange@gmail.com>
2019-05-03 08:59:41 -0600

    Updates

---
+++ precache-manifest.55c1cb413fe76e7d02b5d023e22d98d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60305c6891d12e4950d7a9b67a441fd4",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "041d75b88a346e5bd56f",
+    "url": "/react-tutorial/static/css/main.b006c5fb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-tutorial/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "041d75b88a346e5bd56f",
+    "url": "/react-tutorial/static/js/main.119b6b8e.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
